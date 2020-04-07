6f83e0a9d45509c39edfb57c1a05ecd3fa790627
Ronmel Ross <ronmel_ross@ligph.com>
2020-04-08 00:20:20 +0800

    [30] Understanding Foreign Keys And Database Factories

--- app/Article.php
+++ app/Article.php
@@ -14,8 +14,12 @@ class Article extends Model
         return route('articles.show', $this);
     }
 
-    public function user()
+    // Renamed collection to match the real intent of business logic.
+    // author() in this instance although constructed as a function
+    // is used as a collection property for this model.
+    public function author()
     {
-        # code...
+        // added second parameter which is the FK for User table
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
